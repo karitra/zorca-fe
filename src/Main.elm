0050f44c0c880542ab280ae7ee9c9cd69082e9e8
karitra <karev.alexandre@gmail.com>
2017-11-16 19:37:06 +0300

    chore: move assets folder

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,24 @@
 import Html exposing(..)
 import Html.Attributes exposing(href, class, style)
 
-import Json.Decode exposing (..)
+import Http
+
+-- import Json.Decode as Decode
+import Json.Decode exposing(..)
 import Json.Decode.Pipeline exposing (decode, required)
 
 import Time exposing (Time, second)
 
-import Core exposing(Dict)
+import Dict exposing(..)
+
+import Navigation exposing(Location)
+
+import Bootstrap.Navbar as Navbar
+import Bootstrap.Grid as Grid
 
 
 poll_interval_sec: number
-poll_interval_sec = 20
+poll_interval_sec = 3 -- 20
 
 -- Model
 
@@ -27,12 +35,15 @@ type alias Info = {
     uptime: Int,
     uuid: String,
     version: String,
-    update_timestamp: Int,
+    update_timestamp: Int
+}
+
+type alias DummyOrca = {
+    endpoint: Endpoint
 }
 
 type alias Orca = {
     -- TODO
-    ph: String,
     endpoints: List Endpoint,
     info: Info,
     state: Dict String StateRecord
@@ -42,27 +53,49 @@ type alias OrcasPod = Dict String Orca
 
 type alias Model = {
     content: String,
-    cnt: Int
+    cnt: Int,
+    nav_state: Navbar.State
 }
-
-model = {
-    content = "init",
-    cnt = 0}
 
 -- Update
 
 type Msg =
-    Self |
-    Tick Time
+    Self
+    | Tick Time
+    | UrlChange Location
+    | NavMsg Navbar.State
+    | NewPod (Result Http.Error String)
 
 
 update msg model =
     case msg of
         Self -> (model, Cmd.none)
         Tick time ->
-            Debug.log "Boo"
-            ( {model | cnt = model.cnt + 1} , Cmd.none)
+            let _ = Debug.log "tick" time
+            in
+            ({model | cnt = model.cnt + 1} , get_pod)
 
+        NavMsg state ->
+            ({model | nav_state = state} , Cmd.none)
+        UrlChange location ->
+            (model, Cmd.none)
+        NewPod (Ok pod) ->
+            let _ = Debug.log "get pod" pod
+            in
+            (model, Cmd.none)
+        NewPod (Err e) ->
+            let _ = Debug.log "err" e
+            in
+            (model, Cmd.none)
+
+get_pod =
+    let url = "api/v1/orcas"
+        request = Http.getString url
+    in
+        Http.send NewPod request
+
+--decode_pod =
+--    Decode.at ["a"] Decode.String
 
 -- Subscriptions
 
@@ -73,19 +106,44 @@ subscriptions model =
 
 view model =
     div
-        [ style [("padding", "2rem")] ]
+        []
         [
-            text model.content,
-            text (toString model.cnt)
+            menu model,
+            main_view model
         ]
 
+
+menu model =
+    Navbar.config NavMsg
+        |> Navbar.withAnimation
+        |> Navbar.container
+        |> Navbar.brand [ href "#" ] [ text "Harpoon" ]
+        |> Navbar.view model.nav_state
+
+
+main_view model =
+    Grid.container [] <|
+        [
+            Grid.row []
+            [
+                Grid.col [] [
+                    text (toString model.cnt)
+                ]
+            ]
+        ]
 
 -- Main
 
 main =
+    let (nav_state, cmd) = Navbar.initialState NavMsg in
+    let model = {
+        content = "init",
+        cnt = 0,
+        nav_state = nav_state }
+    in
     Html.program
     {
-        init = (model, Cmd.none),
+        init = (model, cmd),
         view = view,
         update = update,
         subscriptions = subscriptions
